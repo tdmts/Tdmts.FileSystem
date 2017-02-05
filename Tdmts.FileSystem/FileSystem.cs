a45b5d74ddb6108236a284e99c2d584b2ed27395
Tom Demets <tom_demets@outlook.com>
2017-02-05 12:45:13 +0100

    20170205

---
+++ Tdmts.FileSystem/FileSystem.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.AccessCache;
+using Windows.Storage.Pickers;
+
+namespace Tdmts.FileSystem
+{
+    public class FileSystem
+    {
+        private static FileSystem _instance;
+
+        private FileSystem() { }
+
+        public static FileSystem getInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new FileSystem();
+            }
+            return _instance;
+        }
+
+        public async Task<StorageFolder> GetStorageFolder(string fileTypeFilter)
+        {
+            FolderPicker folderPicker = new FolderPicker();
+            folderPicker.FileTypeFilter.Add(fileTypeFilter);
+            StorageFolder storageFolder = await folderPicker.PickSingleFolderAsync();
+            StorageApplicationPermissions.FutureAccessList.Add(storageFolder);
+            return storageFolder;
+        }
+
+        public async Task<StorageFile> GetStorageFile(string extension)
+        {
+            FileOpenPicker filePicker = new FileOpenPicker();
+            filePicker.FileTypeFilter.Add(extension);
+            StorageFile storageFile = await filePicker.PickSingleFileAsync();
+            StorageApplicationPermissions.FutureAccessList.Add(storageFile);
+            return storageFile;
+        }
+    }
+}
